bb3d549adb2ed92e6abfe9c8436b436dcaad3ef0
“EldersonMezzomo” <eldersonmezzomo@hotmail.com>
2022-10-09 11:06:36 -0300

    organizando_codigo_padrao_BEM_conteudoPrincipal

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+/*SAÍDAS DE DADOS*/
+
+/*alert('Teste 1'); /*inserindo alerta*/
+
+/*document.getElementById("teste").innerHTML="Teste 2"; /*inserindo HTML*/
+
+/*document.write('Teste 3'); /*mais utilizado para testes*/
+
+/*console.log('Teste 4'); /*aparece internamente para codificador*/
+
+/*let a,b,c; a=5; b=6; c=8;*//*declaração de variável*/
+
+/*function minhafuncao(){document.write(5 + 5);}*/
+/*declarações em bloco de códigos*/
+
+/*SINTAXE*/
+
+/*tipos de dados=dentro de aspas duplas "" entendido como texto=string
+"case sensitive"
+*/
+
+
+/*variáveis= var let const
+
+var a = 1;
+var b = 5;
+var primeiroResultado = a + b;
+console.log(primeiroResultado);
+
+let = não permite re-declarar=alterar, mas funciona com valor diferente dentro de um escopo (bloco);
+
+
+
+*/
+
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
